3b2241394de244a596842117446e8d995c025f76
Shani Almog <killerdrum@gmail.com>
2020-04-15 18:51:15 +0300

    add spinner to login page while fetching data

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -4,6 +4,7 @@ import { useHistory } from "react-router-dom"
 
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 const Login = () => {
 
@@ -13,7 +14,7 @@ const Login = () => {
     const [password, setPassword] = useState('')
     const [fetchData, setFetchData] = useState(false)
     const [fetchError, setFetchError] = useState(null)
-    // const [loggedIn, setLoggedIn ] = useState('')
+    const [loggedIn, setLoggedIn] = useState('')
 
     const handleSubmit = (event) => {
         event.preventDefault()
@@ -33,7 +34,7 @@ const Login = () => {
         const requestLogin = {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({username, password})
+            body: JSON.stringify({ username, password })
         }
         console.log(requestLogin)
         await fetch(`/users/login`, requestLogin)
@@ -41,7 +42,7 @@ const Login = () => {
                 response => {
                     if (response.ok) {
                         setFetchData(false)
-                        return history.push('/events')  
+                        return history.push('/events')
                     } else {
                         setFetchData(false)
                         console.log(response)
@@ -60,35 +61,41 @@ const Login = () => {
     return (
         <div>
             <h1>Login page</h1>
-            <form onSubmit={handleSubmit} noValidate>
-                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: '30px'}}>
-                    <div>
-                        <TextField
-                            style={{ width: '200px' }}
-                            required
-                            id="standard-required"
-                            label="Username"
-                            name="username"
-                            value={username}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div>
-                        <TextField required
-                            style={{ marginBottom: '60px', width: '200px' }}
-                            id="standard-password-input"
-                            label="Password"
-                            name="password"
-                            type="password"
-                            value={password}
-                            onChange={handleChange}
-                        />
-                    </div>
+            {fetchData ?
+                <CircularProgress color="secondary" />
+                :
+                <div>
+                    <form onSubmit={handleSubmit} noValidate>
+                        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', marginTop: '30px' }}>
+                            <div>
+                                <TextField
+                                    style={{ width: '200px' }}
+                                    required
+                                    id="standard-required"
+                                    label="Username"
+                                    name="username"
+                                    value={username}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div>
+                                <TextField required
+                                    style={{ marginBottom: '60px', width: '200px' }}
+                                    id="standard-password-input"
+                                    label="Password"
+                                    name="password"
+                                    type="password"
+                                    value={password}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                        </div>
+                        <div className="button">
+                            <Button onClick={LogIn} variant="contained">login</Button>
+                        </div>
+                    </form>
                 </div>
-                <div className="button">
-                    <Button onClick={LogIn} variant="contained">login</Button>
-                </div>
-            </form>
+            }
         </div>
     )
 }
